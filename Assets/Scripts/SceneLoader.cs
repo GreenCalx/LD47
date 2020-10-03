3e3383be332e4f4bfffe6f39da9bac88f9518ea4
GreenCalx <romain.schoenberger@gmail.com>
2020-10-03 11:56:30 +0200

    scenes 2 @GreenCalx

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string scene_to_load;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool key_pressed = Input.anyKey;
+        if (key_pressed)
+            SceneManager.LoadScene( scene_to_load, LoadSceneMode.Single);
+    }
+}
