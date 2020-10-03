dfbe7fbf9a06a00e29c811117a31196f71ef328d
GreenCalx <romain.schoenberger@gmail.com>
2020-10-03 14:17:54 +0200

    tile objects and such @GFreenCAlx

---
+++ Assets/Scripts/ActivableObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivableObject : MonoBehaviour
+{
+    public bool isTriggered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTriggered = false;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ButtonTile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTile : MonoBehaviour
+{
+    public GameObject activable;
+    private ActivableObject activableObject;
+
+    void Start()
+    {
+        activableObject = activable.GetComponent<ActivableObject>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        activableObject.isTriggered = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        activableObject.isTriggered = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        activableObject.isTriggered = false;
+    }
+}
