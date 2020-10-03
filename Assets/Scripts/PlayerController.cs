e6231455a74f3388ea363fc55b6523cca11e3f15
Toffanim <marc.toffanin@gmail.com>
2020-10-03 13:30:41 +0200

    Added breaking from last loop

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -13,6 +14,9 @@ public class PlayerController : MonoBehaviour
     public float Speed = 0.5f;
 
     public bool IsLoopedControled = false;
+    bool HasAlreadyBeenBreakedFrom = false;
+
+    public GameObject PlayerPrefab; // needed to create new players when breaking from the loop
 
     // Start is called before the first frame update
     void Start()
@@ -50,6 +54,28 @@ public class PlayerController : MonoBehaviour
                     CurrentDirection = Direction.UP;
                 if (Up < 0)
                     CurrentDirection = Direction.DOWN;
+            }
+        } else
+        {
+            if(Input.GetKeyDown(KeyCode.B) && !HasAlreadyBeenBreakedFrom)
+            {
+                // break from the loop
+                HasAlreadyBeenBreakedFrom = true;
+                // create a new player at current position
+                var GO = Instantiate(PlayerPrefab, this.gameObject.transform.position, Quaternion.identity);
+                GO.GetComponent<PlayerController>().PlayerPrefab = PlayerPrefab;
+                var SpriteRender = GO.GetComponent<SpriteRenderer>();
+                if(SpriteRender)
+                {
+                    SpriteRender.color = UnityEngine.Random.ColorHSV();
+                }
+
+                var Looper = GO.GetComponent<Loop>();
+                if(Looper)
+                {
+                    Looper.IsRecording = true;
+                    Looper.StartPosition = GO.transform.position;
+                }
             }
         }
     }
