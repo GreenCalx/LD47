8981dd374e7a089366ca973662871db015892211
Etienne Lomet <Etienne.Lomet@dirac.com>
2020-10-03 12:17:03 +0200

    first level design concepts, tuto

---
+++ LevelDesign/sprites.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="sprites" tilewidth="64" tileheight="64" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="sprites/button.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="sprites/crate.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="sprites/door_closed.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="sprites/door_open.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="sprites/no_break.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="sprites/player.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="sprites/pressure_plate.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="sprites/wall_horizontal.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="sprites/wall_vertical.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="sprites/teleporter.png"/>
+ </tile>
+</tileset>
