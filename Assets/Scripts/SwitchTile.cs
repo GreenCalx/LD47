cab5a17356b084319e710e38cab398a979965a40
GreenCalx <romain.schoenberger@gmail.com>
2020-10-03 21:12:28 +0200

    FakeExit, Activator new objet hierarchy and switch tile @GreenCalx

--- Assets/Scripts/SwitchTile.cs
+++ Assets/Scripts/SwitchTile.cs
@@ -2,30 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchTile : MonoBehaviour
+public class SwitchTile : ActivatorObject
 {
-    public GameObject activable;
-    
-    public SIGNAL_KEYS signalKey;
-    private ActivableObject activableObject;
 
     void Start()
     {
-        activableObject = activable.GetComponent<ActivableObject>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        activableObject.listen(signalKey);
+        foreach( ActivableObject ao in activableObjects )
+            ao.listen(signalKey);   
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        // button doesn't change
+        // switch doesn't change
     }
 
     private void OnTriggerExit2D(Collider2D other)
     { 
+        // switch doesn't change
     }
 
 }
