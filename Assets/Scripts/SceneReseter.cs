f96355a88bc94e9226ecf8ddd137d0e56755df22
GreenCalx <romain.schoenberger@gmail.com>
2020-10-05 00:52:34 +0200

    hfix

--- Assets/Scripts/SceneReseter.cs
+++ Assets/Scripts/SceneReseter.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneReseter : MonoBehaviour
 {
+        public string scene_to_load;
     // Start is called before the first frame update
     void Start()
     {
