93d69f5e4fd62a24a4baa3f38962417bfdfd436f
Etienne Lomet <Etienne.Lomet@dirac.com>
2020-10-04 04:34:29 +0200

    fixed player starting positions implemented switch_tile change of sprite

--- Assets/Scripts/SwitchTile.cs
+++ Assets/Scripts/SwitchTile.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class SwitchTile : ActivatorObject
 {
 
+    public bool isSwitched = false;
+    public Sprite spriteSwitched;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         foreach( ActivableObject ao in activableObjects )
-            ao.listen(signalKey, this);   
+            ao.listen(signalKey, this);
+
+        if (!isSwitched)
+        {
+            isSwitched = true;
+            SpriteRenderer sr = GetComponentInChildren<SpriteRenderer>();
+            if (!!sr)
+                sr.sprite = spriteSwitched;
+        }
     }
 
     private void OnTriggerStay2D(Collider2D other)
