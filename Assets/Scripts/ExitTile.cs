4c89ebf8a3dcaca23be5a57d2adc1e47cc54df12
GreenCalx <romain.schoenberger@gmail.com>
2020-10-05 21:39:18 +0200

    exit tile now go to nxt level and end game screen if last level @GreenCalx

--- Assets/Scripts/ExitTile.cs
+++ Assets/Scripts/ExitTile.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ExitTile : MonoBehaviour
 {
+    private const string LEVEL_NAME_PREFIX = "LEVEL";
+    private const string END_SCENE = "EndGame";
 
+    private const int MAX_LEVELS = 8;
     public int current_level_exit_index;
     // Start is called before the first frame update
     void Start()
@@ -24,8 +27,14 @@ public class ExitTile : MonoBehaviour
         if ( PC)
         {
             GetComponentInChildren<AudioSource>().Play();
-            SceneManager.LoadScene( "LevelSelectorScene", LoadSceneMode.Single);
             LevelProgress.setProgress( current_level_exit_index, true);
+
+            if (current_level_exit_index >= MAX_LEVELS)
+            {
+                SceneManager.LoadScene( END_SCENE, LoadSceneMode.Single);
+            }
+            string scene_to_load = LEVEL_NAME_PREFIX + ( current_level_exit_index + 1 ) ;
+            SceneManager.LoadScene( scene_to_load, LoadSceneMode.Single);
         }
     }
 }
