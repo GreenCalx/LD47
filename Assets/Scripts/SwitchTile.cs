0c6f0228cf1f37ae8df2fb44ad9209b8318a5765
Toffanim <marc.toffanin@gmail.com>
2020-10-20 11:30:49 +0200

    witch now resets and are rightly checking for anything on them, we should probably do the same thing for buttons

--- Assets/Scripts/SwitchTile.cs
+++ Assets/Scripts/SwitchTile.cs
@@ -7,12 +7,15 @@ public class SwitchTile : ActivatorObject
 
     public bool isSwitched = false;
     public Sprite spriteSwitched;
+    private List<GameObject> objectsOnSwitch = new List<GameObject>();
+    public Sprite spriteNotSwitched;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-
         if (!isSwitched)
         {
+            objectsOnSwitch.Add(other.gameObject);
+
             foreach( ActivableObject ao in activableObjects )
                 ao.listen(signalKey, this, true);
 
@@ -26,12 +29,25 @@ public class SwitchTile : ActivatorObject
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        // switch doesn't change
     }
 
     private void OnTriggerExit2D(Collider2D other)
-    { 
-        // switch doesn't change
+    {
+        objectsOnSwitch.Remove(other.gameObject);
+
+        // switch is reseted when going out of it
+        // and nothing is on it anymore
+        if (isSwitched && objectsOnSwitch.Count == 0)
+        {
+            foreach( ActivableObject ao in activableObjects )
+                ao.listen(signalKey, this, false);
+
+            isSwitched = false;
+
+            SpriteRenderer sr = GetComponentInChildren<SpriteRenderer>();
+            if (!!sr)
+                sr.sprite = spriteNotSwitched;
+        }
     }
 
 }
