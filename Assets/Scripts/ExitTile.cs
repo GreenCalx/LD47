63529472737e7fc744072e5e8f6338335dc71f64
GreenCalx <romain.schoenberger@gmail.com>
2020-10-26 00:57:35 +0100

    exit tile and world

--- Assets/Scripts/ExitTile.cs
+++ Assets/Scripts/ExitTile.cs
@@ -16,6 +16,10 @@ public class ExitTile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+    }
+
+    void Awake()
+    {
         getLevelAndStageID();
     }
 
