8d7d021bbd539c9ba592c288cc7410b5e9cc5dca
GreenCalx <romain.schoenberger@gmail.com>
2021-01-29 01:49:13 +0100

    timeline skin update + stage skin udate

---
+++ Assets/Scripts/StageBackground.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class StageBackground : MonoBehaviour
+{
+    public float max_alpha;
+    public float min_alpha;
+
+    private Image __image;
+
+    private double __elapsed_time; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        __image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        __elapsed_time += Time.deltaTime;
+/* 
+        var alpha =  max_alpha - Mathf.Sin(0);
+        __image.color = new Color( __image.color.r, 
+                                   __image.color.g,
+                                   __image.color.b,
+                                   alpha); */
+    }
+}
