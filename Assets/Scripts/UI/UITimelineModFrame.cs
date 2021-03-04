5751e602c3e8152a62db513581db8c328ff56234
GreenCalx <romain.schoenberger@gmail.com>
2021-03-05 00:44:58 +0100

    ui bg fix

--- Assets/Scripts/UI/UITimelineModFrame.cs
+++ Assets/Scripts/UI/UITimelineModFrame.cs
@@ -6,10 +6,6 @@ using UnityEngine.UI;
 public class UITimelineModFrame : MonoBehaviour
 {
 
-    public Color record_color;
-    public Color rewind_color;
-    public Color play_color;
-
     public float alpha_damp;
 
     private Image[] __children_img;
@@ -28,32 +24,16 @@ public class UITimelineModFrame : MonoBehaviour
         forward_tick = false;
     }
 
-    private void trySetColor( Color iColor )
-    {
-        Color dampened_color = iColor;
-        dampened_color.a -= alpha_damp;
-
-        if (__self_img.color == dampened_color )
-            return;
-
-        if (!!__self_img)
-            __self_img.color = dampened_color;
-        foreach( Image im in __children_img ) { im.color = dampened_color; }
-    }
-
     public void setToRecord()
     {
-        trySetColor(record_color);
     }
 
     public void setToReplay()
     {
-        trySetColor(play_color);
     }
 
     public void setToRewind()
     {
-        trySetColor(rewind_color);
     }
 
     // Update is called once per frame
