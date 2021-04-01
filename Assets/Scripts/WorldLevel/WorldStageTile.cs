846ce2438c392ff0c97b8b3f318ca106ed4c853d
GreenCalx <romain.schoenberger@gmail.com>
2021-04-01 18:05:37 +0200

    stage tile skin change son world upon completion

---
+++ Assets/Scripts/WorldLevel/WorldStageTile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldStageTile : MonoBehaviour
+{
+
+    public Sprite stage_undone;
+    public Sprite stage_done;
+
+    private SpriteRenderer __sr;
+    private Stage __stage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        __sr = GetComponent<SpriteRenderer>();
+        __stage = GetComponent<Stage>();
+        if (!!__sr && __stage)
+            __sr.sprite = LevelProgress.getCompletion( __stage.level_to_load, __stage.stage_to_load) ? stage_done : stage_undone;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
